1479fa8cf8a376cfb0b914333755bae246e66ac6
RossDuquette <rduquette423@gmail.com>
2016-12-08 16:45:31 -0500

    Moving items to header file

--- Solver.cpp
+++ Solver.cpp
@@ -1,15 +1,7 @@
-#include "Element.h"
+#include "Solver.h"
 #include <vector>
 #include <iostream>
 #include <fstream>
-
-using namespace std;
-
-bool check(Element* cell, int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8, int n9);
-void read(Element* cell, string name);
-void write(Element* cell, string name, bool done);
-bool solve(Element* cell);
-bool finished(Element* cell);
 
 int main()
 {	
@@ -240,6 +232,3 @@ bool finished(Element* cell)
 	}
 	return true;
 }
-
-
-
