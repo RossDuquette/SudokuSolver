91cd05af379a535a99589f6d232bf8023391caf4
Ross Duquette <ross.duquette@gastronomous.ca>
2023-08-27 16:58:00 -0400

    Update the main program to run the solver

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-int main()
+#include "sudoku_solver.h"
+
+static void print_help()
 {
-    std::cout << "Hello world" <<std::endl;
+    std::cout << "Must pass in file name as argument\n";
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc == 2) {
+        SudokuSolver solver;
+        solver.load_file(argv[1]);
+        solver.solve();
+        solver.display_output();
+    } else {
+        print_help();
+    }
     return 0;
 }
